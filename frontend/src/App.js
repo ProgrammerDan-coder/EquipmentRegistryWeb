559e78a42121097c7dba4c2021e47f0d117f0bfc
kostyukevich.dan@gmail.com <kostyukevich.dan@gmail.com>
2022-07-06 11:27:27 +0300

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,45 @@
+import React, {useState} from 'react';
+import './styles/App.css';
+import companyLogo from './images/BSTUImg.jpg'
+import searchIcon from './images/search.jpg'
+
+
+function App() {
+
+  
+
+  
+
+  return (
+    <div className="App">
+    
+    <div className="post">
+      <img className='img_log' src={companyLogo} alt="BSTU logo"/>
+        
+          <h1 className='reestr'>Реестр оборудования БГТУ</h1>
+       
+          <div className='block'>
+            <div className='registr'>Регистрация</div>
+            <div className='auth'>Вход</div>
+          </div>
+    </div>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <div className='main_tite'>
+      <p className='p'>Главная</p>
+      <p className='p'> Добавить оборудования</p>
+    </div>
+    <div className='search_block'>
+    <h2 className='equipment_text'>Оборудование</h2>
+    {/* <img className='img_log' src={searchIcon} alt="BSTU logo"/> */}
+    <input className='input' type="text" />
+    <button className='button_input'>Поиск &gt;</button>
+    </div>
+    </div>
+  );
+}
+
+export default App;
